4657ddb7a284de412c1e937aa634156c262a1170
Divakar M <diva132006@gmail.com>
2025-02-12 22:57:10 +0530

    Update simple_page.py

--- simple_page.py
+++ simple_page.py
@@ -1,6 +1,10 @@
 import os
 import psycopg2
 from flask import Flask, request, render_template_string
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Initialize the Flask app
 app = Flask(__name__)
